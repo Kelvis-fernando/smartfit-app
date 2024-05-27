9dbace57d346c39208ebde98a2e1bfe7e3c3fdf0
KelvisDelapena <kelvisdelapena@gmail.com>
2024-05-27 13:39:10 -0300

    feat: create service and add a http get requuest to get all characters

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -3,6 +3,14 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Location } from 'src/app/interfaces/location.interface';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 
+const OPENING_HOURS = {
+  morning: { first: '06', last: '12' },
+  afternoon: { first: '12', last: '18' },
+  night: { first: '18', last: '23' },
+};
+
+type HourIndexes = 'morning' | 'afternoon' | 'night';
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -26,7 +34,59 @@ export class FormsComponent {
     });
   }
 
+  formatWeekday(day: number) {
+    switch (day) {
+      case 0:
+        return 'Dom.';
+      case 6:
+        return 'Sáb.';
+      default:
+        return 'Seg. à Sex.';
+    }
+  }
+
+  filterUnits(unit: Location, open_hour: string, close_hour: string) {
+    let open_hour_filter = parseInt(open_hour, 10);
+    let close_hour_filter = parseInt(close_hour, 10);
+
+    let todays_weekday = this.formatWeekday(new Date().getDay());
+
+    for (let i = 0; i < unit.schedules.length; i++) {
+      let schedule_hour = unit.schedules[i].hour;
+      let schedule_weekday = unit.schedules[i].weekdays;
+
+      if (todays_weekday === schedule_weekday) {
+        if (schedule_hour !== 'Fechada') {
+          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ');
+          let unit_open_hour_int = parseInt(
+            unit_open_hour.replace('h', ''),
+            10
+          );
+          let unit_close_hour_int = parseInt(
+            unit_open_hour.replace('h', ''),
+            10
+          );
+
+          if (
+            unit_open_hour_int >= open_hour_filter &&
+            unit_close_hour_int >= close_hour_filter
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+
+      return false;
+    }
+  }
+
   onSubmit(): void {
+    const OPEN_HOUR =
+      OPENING_HOURS[this.formGroup.value.hour as HourIndexes].first;
+    const CLOSE_HOUR =
+      OPENING_HOURS[this.formGroup.value.hour as HourIndexes].last;
     if (!this.formGroup.value.showClosed) {
       this.filteredResults = this.results.filter(
         (location) => location.opened === true
